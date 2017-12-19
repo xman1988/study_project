87116544b24944c17c2723d38add7ab1477254e1
Chivkin Andrey <xman.88@mail.ru>
2017-12-20 02:46:10 +0300

    Поправки: 1. Исключен protected $link везде, кроме родного класса. 2. Создана функция отправки запросов в БД(чтобы сделать см.пункт №1).

--- counter.php
+++ counter.php
@@ -26,26 +26,27 @@ class Counter{
     public function getCounterData() {
         $ip = $_SERVER['REMOTE_ADDR'];
         $ip_my = $_SERVER['SERVER_ADDR'];
+
         $this->sql = "SELECT COUNT(ip) FROM `hits`
-    INNER JOIN `ip`
-    ON `hits`.`ip_id` = `ip`.`id`";
-        echo "<br><br>" . $this->sql;
+                      INNER JOIN `ip`
+                      ON `hits`.`ip_id` = `ip`.`id`";
+        //echo "<br><br>" . $this->sql;
         $result[]= $this->db->dbSelectToArray($this->db->getQuery($this->sql));
 
         $this->sql = "SELECT COUNT(*) FROM ip WHERE ip = '$ip'";
-        echo "<br>".$this->sql."<br>";
+        //echo "<br>".$this->sql."<br>";
         $result[]= $this->db->dbSelectToArray($this->db->getQuery($this->sql));
 
         $this->sql = "SELECT COUNT(*) FROM ip WHERE ip = '$ip_my'";
-        echo $this->sql."<br>";
+        //echo $this->sql."<br>";
         $result[]= $this->db->dbSelectToArray($this->db->getQuery($this->sql));
 
         $this->sql = "SELECT COUNT(DISTINCT ip) FROM ip";
-        echo $this->sql."<br>";
+        //echo $this->sql."<br>";
         $result[]= $this->db->dbSelectToArray($this->db->getQuery($this->sql));
 
         $this->sql = "SELECT DISTINCT ip, COUNT(*) FROM ip GROUP BY ip";
-        echo $this->sql."<br>";
+        //echo $this->sql."<br>";
         $result[]= $this->db->dbSelectToArray($this->db->getQuery($this->sql));
 
         return $result;
